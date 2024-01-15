4d46114b71a25e754be8a06ba67168f4a7c1a400
raymeng05 <raybenmeng@gmail.com>
2024-01-14 22:03:15 -0500

    changed stuff 2

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "pngazwqjsepghlhtabyd.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
